c8082d29ca1bdba08ec43c53fbdad6f73c119ce4
stas <stas37951@gmail.com>
2023-08-29 11:10:16 +0300

    gitignore, pep8

--- SPA_comments/settings.py
+++ SPA_comments/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mt35-y(upbtuof7t4lj*v)w2h$t$ww5m97p0s8hv^hj$+(fbnv'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-mt35-y(upbtuof7t4lj*v)w2h$t$ww5m97p0s8hv^hj$+(fbnv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
